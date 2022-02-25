23fb4365f0c084ef82d0929bf574aa53c6b1d7de
99angelrm <99angelrm@gmail.com>
2022-02-25 00:17:05 -0600

    Check router

--- src/PresentationApp.jsx
+++ src/PresentationApp.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import MainScreen from "./components/main/MainScreen";
 import ProjectsScreen from "./components/projects/ProjectsScreen";
 
 const PresentationApp = () => {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
                 <Route path="/me" element={<MainScreen />} />
                 <Route path='/me/projects' element={<ProjectsScreen/>}/>
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     );
 };
 
