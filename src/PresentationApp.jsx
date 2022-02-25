55feea347d630e78b396b7ede2641b0f12c9c655
99angelrm <99angelrm@gmail.com>
2022-02-25 00:31:53 -0600

    Changes un router

--- src/PresentationApp.jsx
+++ src/PresentationApp.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainScreen from "./components/main/MainScreen";
 import ProjectsScreen from "./components/projects/ProjectsScreen";
 
 const PresentationApp = () => {
     return (
-        <HashRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
             <Routes>
                 <Route path="/me" element={<MainScreen />} />
                 <Route path='/me/projects' element={<ProjectsScreen/>}/>
             </Routes>
-        </HashRouter>
+        </BrowserRouter>
     );
 };
 
